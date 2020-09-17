cb1bdaba22aa1b0cf6ebe9f3c988b0480cc4bcd6
fabiocalca <62962884+fabiocalca@users.noreply.github.com>
2020-09-17 19:12:14 -0300

    Add files via upload

---
+++ plot4.R
@@ -0,0 +1,56 @@
+# Load the necessary libraries
+library(readr)
+library(dplyr)
+library(lubridate)
+dataDir <- "./data"
+zipFile <- "./data/household_power_consumption.zip"
+dataFile <- "./data/household_power_consumption.txt"
+# Create a directory named 'data' if not already present.
+# Download and extract the .zip file,
+# before reading the file into a data table
+if(!file.exists(dataDir)) {
+  dir.create(dataDir)
+  fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+  download.file(url = fileUrl, destfile = zipFile)
+  unzip(zipFile, exdir = dataDir)
+}
+# Save the variable names to index it later since we are going to skip rows
+savedNames <- c("Date","Time","Global_active_power","Global_reactive_power",
+                "Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+
+# Read the file
+firstWanted <- 66638 # Line of date 2007-02-01 at 00:00
+number_of_lines <- 60*24*2 # 60 minutes times 24 hours times 2 days
+data <- read_delim(dataFile,delim=';',col_names=savedNames,
+                   skip = firstWanted-1,n_max=number_of_lines,na='?')
+
+# Fix dates with lubridate and create a new variable with date and time together
+data <- data %>%
+  mutate(Date=dmy(data$Date))
+data <- data %>%
+  mutate(datetime=as.POSIXct(paste(data$Date,data$Time)))
+
+# Open a png device
+png(filename = 'plot4.png')
+
+# Set parameter to put 4 graphs together
+par(mfcol=c(2,2))
+
+# Create the first plot as in plot2
+with(data,plot(datetime,Global_active_power,type='l',xlab='',ylab='Global Active Power (kilowatts)'))
+
+# Create the second plot as in plot3, but without the legend box line
+with(data,plot(datetime,Sub_metering_1,type='l',xlab = '',ylab='Energy sub metering'))
+with(data,lines(datetime,Sub_metering_2,col='red'))
+with(data,lines(datetime,Sub_metering_3,col='blue'))
+legend("topright",legend=c('sub_metering_1','sub_metering_2','sub_metering_3'),
+       col=c('black','red','blue'),lwd=1,box.lwd = 0)
+
+# Create the third plot (Voltage VS datetime)
+with(data,plot(datetime,Voltage,type='l'))
+
+# Create the last plot
+with(data,plot(datetime,Global_reactive_power,type='l'))
+
+# Close the graph device
+dev.off()
